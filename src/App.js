f981448f3b748f8f053f8f6568662022f4117f3c
Drew Stone <drewstone329@gmail.com>
2018-03-16 17:54:30 -0400

    Start including dharma

--- src/App.js
+++ src/App.js
@@ -1,24 +1,46 @@
 // @flow
 import React, { Component } from 'react'
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
-// import AccountsContract from '../build/contracts/Accounts.json'
+import Dharma from "@dharmaprotocol/dharma.js";
+
 import getWeb3 from './utils/getWeb3'
 
 import Landing from './Landing'
 import LoadingPage from './LoadingPage'
-// import StudentSignup from './StudentSignup'
-// import FunderSignup from './FunderSignup'
-// import SideBar from './SideBar'
-// import StudentWallet from './StudentWallet'
-// import StudentProfile from './StudentProfile'
-// import StudentTransactions from './StudentTransactions'
-// import FunderHome from './FunderHome'
+
 
 class App extends Component {
   state = {
     web3: null,
     // AccountsInstance: null,
     // accounts: null
+  }
+
+  async instantiateDharma() {
+    const networkId = await promisify(this.state.web3.version.getNetwork)();
+    const accounts = await promisify(this.state.web3.eth.getAccounts)();
+    
+    if (!(networkId in DebtKernel.networks &&
+          networkId in RepaymentRouter.networks &&
+          networkId in TokenTransferProxy.networks &&
+          networkId in TokenRegistry.networks && 
+          networkId in DebtToken.networks &&
+          networkId in TermsContractRegistry.networks)) {
+        throw new Error("Cannot find Dharma smart contracts on current Ethereum network.");
+    }
+    
+    const dharmaConfig = {
+        kernelAddress: DebtKernel.networks[networkId].address,
+        repaymentRouterAddress: RepaymentRouter.networks[networkId].address,
+        tokenTransferProxyAddress: TokenTransferProxy.networks[networkId].address,
+        tokenRegistryAddress: TokenRegistry.networks[networkId].address,
+        debtTokenAddress: DebtToken.networks[networkId].address,
+        termsContractRegistry: TermsContractRegistry.networks[networkId].address
+    }
+    
+    const dharma = new Dharma(this.state.web3.currentProvider, dharmaConfig);
+    
+    this.setState({ dharma, accounts });
   }
 
   async componentWillMount() {
