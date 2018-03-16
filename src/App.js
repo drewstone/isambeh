d17be9c06821a8c7063708cb05b94bda23173937
Gilad <freeslugs@gmail.com>
2018-03-16 14:54:02 -0400

    init react repo

---
+++ src/App.js
@@ -0,0 +1,56 @@
+// @flow
+import React, { Component } from 'react'
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+// import AccountsContract from '../build/contracts/Accounts.json'
+import getWeb3 from './utils/getWeb3'
+
+import Landing from './Landing'
+// import StudentSignup from './StudentSignup'
+// import FunderSignup from './FunderSignup'
+// import SideBar from './SideBar'
+// import StudentWallet from './StudentWallet'
+// import StudentProfile from './StudentProfile'
+// import StudentTransactions from './StudentTransactions'
+// import FunderHome from './FunderHome'
+
+class App extends Component {
+  // state = {
+  //   web3: null,
+  //   AccountsInstance: null,
+  //   accounts: null
+  // }
+
+  // async componentWillMount() {
+  //   try {
+  //     const results = await getWeb3;
+  //     this.setState({ web3: results.web3 })
+
+  //     // configure and include the smart contract
+  //     const contract = require('truffle-contract')
+  //     const Accounts = contract(AccountsContract)
+      
+  //     Accounts.setProvider(this.state.web3.currentProvider)
+    
+  //     // Get accounts.
+  //     this.state.web3.eth.getAccounts(async (error, accounts) => {
+  //       const instance = await Accounts.deployed();
+  //       this.setState({ accounts: accounts, AccountsInstance: instance });
+  //     })
+  //   } catch (e) {
+  //     console.log('Error finding web3.')
+  //   }
+  // }
+
+  render() {
+    return (
+      <Router>
+        <div>
+          <Route exact path="/" render={(props) => ( <Landing {...props} {...this.state} /> )} />
+          {/*<Route exact path="/student/signup" render={(props) => ( <StudentSignup {...props} {...this.state} /> )} />*/}
+        </div>
+      </Router>
+    )
+  }
+}
+
+export default App;
